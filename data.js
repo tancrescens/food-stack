1ba6769a049eefc7f98544359369a44a579b9da1
Crescens <2e3crescens@gmail.com>
2024-10-03 22:50:46 +0800

    CREATE: create food Items

--- data.js
+++ data.js
@@ -1,8 +1,20 @@
-let BIN_ID = "66fe9366ad19ca34f8b2127e";
+let BIN_ID = "66feac11e41b4d34e43c5e13";
 let BASE_URL = "https://api.jsonbin.io/v3";
 
 // loads data from db to global array in script.js
 async function loadData() {
-  let response = await axios.get(`${BASE_URL}/b/${BIN_ID}`);
+  let response = await axios.get(`${BASE_URL}/b/${BIN_ID}/latest`);
   return response.data.record; //returns the array that contains food objects
+}
+
+async function createFoodItem(foods, foodName, foodPrice, foodRatings) {
+  let foodItem = {
+    id: foods.length,
+    foodName: foodName,
+    foodPrice: foodPrice,
+    foodRatings: foodRatings,
+  };
+  foods.push(foodItem);
+  console.log("foods", foods);
+  await axios.put(`${BASE_URL}/b/${BIN_ID}`, foods);
 }
--- script.js
+++ script.js
@@ -5,7 +5,20 @@ window.addEventListener("DOMContentLoaded", async function () {
   foods = await loadData();
 
   renderFoodList();
-});
+
+  document
+    .querySelector("#createFoodItemBtn")
+    .addEventListener("click", function () {
+      let foodName = document.querySelector("#foodName").value;
+      let foodPrice = document.querySelector("#foodPrice").value;
+      let foodRatings = document.querySelector(".ratings:checked").value;
+
+      // the addTask function is in data.js
+      createFoodItem(foods, foodName, foodPrice, foodRatings);
+      // re-render all the tasks
+      renderFoodList();
+    });
+}); // end DOMContentLoaded
 
 function renderFoodList() {
   let foodUlEle = document.querySelector("#foods");
@@ -18,7 +31,7 @@ function renderFoodList() {
     let liElement = document.createElement("li");
     liElement.innerHTML = `
       Food: ${foodItem.foodName} <button class="edit">Edit</button> <button class="delete">Delete</button><br />
-      Price: ${foodItem.price} | Ratings: ${foodItem.ratings}⭐
+      Price: ${foodItem.foodPrice} | Ratings: ${foodItem.foodRatings}⭐
       <br />
       <br />
     `;
@@ -26,10 +39,22 @@ function renderFoodList() {
 
     // Edit Button Function
     let editBtn = document.querySelector(".edit");
-    editBtn.addEventListener("click", function () {});
+    editBtn.addEventListener("click", function () {
+      // let newFoodName = prompt("Enter the new food name: ", foodItem.foodName);
+      // let newFoodPrice = prompt(
+      //   "Enter the new food price: ",
+      //   foodItem.foodPrice
+      // );
+      // let newFoodRatings = prompt(
+      //   "Enter the new food ratings: ",
+      //   foodItem.foodRatings
+      // );
+      // updateTask(tasks, t.id, newTaskName, newUrgency, isDone);
+      // renderTasks(); // redraw all the tasks, along with any changes
+    });
 
     // Delete Button Function
     let deleteBtn = document.querySelector(".edit");
     deleteBtn.addEventListener("click", function () {});
   }
-}
+} // end RenderFoodList()
