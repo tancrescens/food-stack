c6d9c570412a03ff2e9506a4350bfe6626e2cbab
Crescens <2e3crescens@gmail.com>
2024-10-03 21:49:52 +0800

    renderFoodList(): render list from global "foods" array

--- script.js
+++ script.js
@@ -8,7 +8,28 @@ window.addEventListener("DOMContentLoaded", async function () {
 });
 
 function renderFoodList() {
-  // let foodListEle = document.querySelector("#foods")
-  // for(let foodItem in foods){
-  // }
+  let foodUlEle = document.querySelector("#foods");
+
+  // empty the task list of all the <li> inside it. To facilitate rendering of list.
+  foodUlEle.innerHTML = ""; // remove all the children inside
+
+  // Display + create functionality of Edit and Delete btn
+  for (let foodItem of foods) {
+    let liElement = document.createElement("li");
+    liElement.innerHTML = `
+      Food: ${foodItem.foodName} <button class="edit">Edit</button> <button class="delete">Delete</button><br />
+      Price: ${foodItem.price} | Ratings: ${foodItem.ratings}⭐
+      <br />
+      <br />
+    `;
+    foodUlEle.appendChild(liElement);
+
+    // Edit Button Function
+    let editBtn = document.querySelector(".edit");
+    editBtn.addEventListener("click", function () {});
+
+    // Delete Button Function
+    let deleteBtn = document.querySelector(".edit");
+    deleteBtn.addEventListener("click", function () {});
+  }
 }
